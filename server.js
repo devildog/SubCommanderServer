3d52c850768c47ecc68328cec9f1fd21c2a90b76
Tim Weaver <tim@burningembersoftware.com>
2012-07-19 17:48:56 -0400

    set port to 8000

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ function ensureAuthenticated(req, res, next) {
 }
 
 
-app.listen(3000, function(){
+app.listen(8000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
